e7a98b676a79f8bd4486ae6692ffeabb1ba3daa3
Christian Josef Aquino <72939231+chrstnjsff@users.noreply.github.com>
2025-02-03 04:19:31 +0800

    Update telescope.lua

--- telescope.lua
+++ telescope.lua
@@ -1,51 +1,11 @@
-require("telescope").setup({
-	defaults = {
-		file_ignore_patterns = { "node_modules", ".git" },
-		vimgrep_arguments = {
-			"rg",
-			"--color=never",
-			"--no-heading",
-			"--with-filename",
-			"--line-number",
-			"--column",
-			"--smart-case",
-		},
-		prompt_prefix = "> ",
-		selection_caret = "> ",
-		entry_prefix = "  ",
-		initial_mode = "insert",
-		selection_strategy = "reset",
-		sorting_strategy = "descending",
-		layout_strategy = "horizontal",
-		layout_config = {
-			horizontal = {
-				mirror = false,
-			},
-			vertical = {
-				mirror = false,
-			},
-		},
-		file_sorter = require("telescope.sorters").get_fuzzy_file,
-		generic_sorter = require("telescope.sorters").get_generic_fuzzy_sorter,
-		path_display = { "truncate" },
-		winblend = 0,
-		border = {},
-		borderchars = { "─", "│", "─", "│", "╭", "╮", "╯", "╰" },
-		color_devicons = true,
-		set_env = { ["COLORTERM"] = "truecolor" }, -- default = nil,
-		file_previewer = require("telescope.previewers").vim_buffer_cat.new,
-		grep_previewer = require("telescope.previewers").vim_buffer_vimgrep.new,
-		qflist_previewer = require("telescope.previewers").vim_buffer_qflist.new,
-	},
-})
-
-vim.api.nvim_set_keymap(
-	"n",
-	"gf",
-	":Telescope find_files search_dirs={'../../rds'}<CR>",
-	{ noremap = true, silent = true }
-)
-
 return {
-	{ "nvim-telescope/telescope.nvim", requires = { { "nvim-lua/plenary.nvim" } } },
+	{
+		"nvim-telescope/telescope.nvim",
+		keys = {
+			{ "<C-p>", "<cmd>Telescope find_files<cr>", desc = "Find Files" },
+			{ "<leader>fg", "<cmd>Telescope live_grep<cr>", desc = "Live Grep" },
+			{ "<leader>fb", "<cmd>Telescope buffers<cr>", desc = "Buffers" },
+			{ "<leader>fh", "<cmd>Telescope help_tags<cr>", desc = "Help Tags" },
+		},
+	},
 }
